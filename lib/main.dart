88770ff0872aca8b5826af6dc5d360ac05c19ed8
Premchand777 <65839523+Premchand777@users.noreply.github.com>
2024-03-15 22:10:54 +0530

    commented app's theme related settings in main.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -14,3 +14,38 @@ void main() {
     ),
   );
 }
+
+// var kColorScheme = ColorScheme.fromSeed(
+//   seedColor: Colors.blue,
+// );
+
+// var kDarkColorScheme = ColorScheme.fromSeed(
+//   brightness: Brightness.dark,
+//   seedColor: const Color.fromARGB(255, 76, 44, 188),
+// );
+
+// void main() {
+//   runApp(
+//     SafeArea(
+//       child: MaterialApp(
+//         theme: ThemeData().copyWith(
+//           colorScheme: kColorScheme,
+//           appBarTheme: const AppBarTheme().copyWith(
+//             backgroundColor: kColorScheme.onPrimaryContainer,
+//             foregroundColor: kColorScheme.primaryContainer,
+//           ),
+//         ),
+//         darkTheme: ThemeData.dark().copyWith(
+//           colorScheme: kDarkColorScheme,
+//           appBarTheme: const AppBarTheme().copyWith(
+//             backgroundColor: kDarkColorScheme.onPrimaryContainer,
+//             foregroundColor: kDarkColorScheme.primaryContainer,
+//           ),
+//         ),
+//         // themeMode: ThemeMode.system,
+//         home: const ExpensesHomeScreenSFW(),
+//         debugShowCheckedModeBanner: false,
+//       ),
+//     ),
+//   );
+// }
