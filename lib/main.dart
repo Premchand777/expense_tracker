913d7ed8a158d94f92549049d1c0a32ef70f34ad
Premchand777 <65839523+Premchand777@users.noreply.github.com>
2024-03-16 15:15:51 +0530

    locked landscape orientation or device rotaion in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,23 @@
 // import flutter pkgs
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 // import project pkgs
 import 'package:expense_tracker/widgets/expenses_home_sfw.dart';
 
 void main() {
-  runApp(
-    const SafeArea(
-      child: MaterialApp(
-        home: ExpensesHomeScreenSFW(),
-        debugShowCheckedModeBanner: false,
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((value) {
+    runApp(
+      const SafeArea(
+        child: MaterialApp(
+          home: ExpensesHomeScreenSFW(),
+          debugShowCheckedModeBanner: false,
+        ),
       ),
-    ),
-  );
+    );
+  });
 }
 
 // var kColorScheme = ColorScheme.fromSeed(
